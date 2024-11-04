c1eee8fa46e8f69929bd4d0237cb3a8457438e5f
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2024-11-04 14:52:56 -0300

    Updating sub and pubs to fit with microAutoware

--- carla_autoware_bridge/aw_bridge.py
+++ carla_autoware_bridge/aw_bridge.py
@@ -41,30 +41,45 @@ class AutowareBridge(Node):
         self._vehicle_status_subscriber = self.create_subscription(
             CarlaEgoVehicleStatus, '/carla/ego_vehicle/vehicle_status',
             self._vehicle_status_callback, qos.qos_profile_sensor_data)
+        
+        # * Publish in Carla_Serial_Bridge to microAutoware
+        # TODO: BRIDGE TO CARLA_SERIAL
         self._steering_status_publisher = self.create_publisher(
-            SteeringReport, '/vehicle/status/steering_status', 1)
+            SteeringReport, '/microautoware/vehicle/status/steering_status', 1)
         
         # Publish a static control_mode
         # Out Bridge is always in autonomous mode, this could be changed later
-        self._vehicle_control_mode_publisher = self.create_publisher(
-            ControlModeReport, '/vehicle/status/control_mode', 1)
+        # ! overridded by microautoware
+        # ! self._vehicle_control_mode_publisher = self.create_publisher(
+        # !    ControlModeReport, '/vehicle/status/control_mode', 1)
         
         self._odometry_subscriber = self.create_subscription(
             Odometry, '/carla/ego_vehicle/odometry',
             self._odometry_callback, qos.qos_profile_sensor_data)
+        
+        
+        # * Publish in Carla_Serial_Bridge to microAutoware
+        # TODO: BRIDGE TO CARLA_SERIAL
         self._velocity_report_publisher = self.create_publisher(
-            VelocityReport, '/vehicle/status/velocity_status', 1)
+            VelocityReport, '/microautoware/vehicle/status/velocity_status', 1)
+        
+        
         # Send directly pose_with_covariance
         # New gnss poser does not support the right conversion anymore
         self._pose_with_cov_publisher = self.create_publisher(
             PoseWithCovarianceStamped, '/sensing/gnss/pose_with_covariance', 1)
         
+        
         # Subscribe autoware ackermann control cmd and send to the PID
-        self._ackermann_control_command_subscriber = self.create_subscription(
-            AckermannControlCommand, '/control/command/control_cmd',
-            self._control_callback, qos.qos_profile_sensor_data)
-        self._ackermann_pub = self.create_publisher(
-            AckermannDrive, '/carla/ego_vehicle/ackermann_cmd', 1)
+        # ! overridded by microautoware
+        # ! self._ackermann_control_command_subscriber = self.create_subscription(
+        # !     AckermannControlCommand, '/control/command/control_cmd',
+        # !     self._control_callback, qos.qos_profile_sensor_data)
+        
+        
+        # ! overridded by microautoware
+        # ! self._ackermann_pub = self.create_publisher(
+        # !     AckermannDrive, '/carla/ego_vehicle/ackermann_cmd', 1)
        
     def _odometry_callback(self, odometry_msg):
         # velocity msg
